fb78e7de0c184c9ab12e3c06c21a86c84e4271c3
f.squarciafico <f.squarciafico@gmail.com>
2022-10-27 21:18:20 +0200

    max_due_num

--- helloword.py
+++ helloword.py
@@ -1,1 +1,13 @@
-print ("hello word");
+# print ("hello word");
+
+# stampa il maggiore di due numeri
+
+def max_num (a,b):
+    
+    if a==b:
+        print ("I due numeri sono uguali")
+    elif a<b:
+        print ("Il numero più grande è: "+ str(b))
+    else :
+        print ("il numero più grande è: "+ str(a))
+
